dd56cb84a959db28274e4aadd0c70c977a98c8a3
aRonnieAlsop <alsop.arron@gmail.com>
2024-08-09 21:25:34 -0700

    'more of just making sure there's a backend to frontend connection'

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Home from './components/Home';
 import Users from './components/Users';
 import CreateUser from './components/CreateUser';
 
@@ -7,9 +9,10 @@ const App = () => {
   return (
     <Router>
       <div>
+        <NavBar />
         <Switch>
           <Route path="/users" component={Users} />
-          <Route path="/post" component={CreateUser} />        
+          <Route path="/create-user" component={CreateUser} />        
         </Switch>
       </div>
     </Router>
