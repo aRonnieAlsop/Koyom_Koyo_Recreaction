00746e8936d3120f3b25b41879f2f1f8f35c743b
aRonnieAlsop <alsop.arron@gmail.com>
2024-08-14 22:08:04 -0700

    'social media icons within'

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './NavBar.css'; 
+
+import faceBookIcon from './../../assets/faceBook.png'
+import instagramIcon from './../../assets/instagram.png';
 
 const NavBar = () => {
     return (
       <nav className="navbar">
         <div className="navbar-left">
         <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
-            <i className="fab fa-facebook-square"></i>
-        </a>
-        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
-            <i className="fab fa-instagram"></i>
-        </a>
+            <img src={faceBookIcon} alt="Facebook" className="social-icon"/>
+          </a>
+          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+            <img src={instagramIcon} alt="Instagram" className="social-icon"/>
+          </a>
         </div>
         <div className="navbar-right">
           <a href="/">Home</a>
