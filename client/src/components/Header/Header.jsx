ca58002cae5175f7e3c3c82583c14ac2aaa11e95
aRonnieAlsop <alsop.arron@gmail.com>
2024-08-14 22:18:51 -0700

    'wip Header + NavBar'

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import NavBar from './../NavBar/NavBar';
-import './Header.css'; // If you have custom styles
+import './Header.css'; 
 
 const Header = () => {
     return (
         <header>
             <NavBar />
-            <h1>Koyom Koyo Parks & Recreation</h1>
+            <h1>KOYOM KOYO</h1>
+            <h2>Parks & Recreation</h2>
         </header>
     );
 };
