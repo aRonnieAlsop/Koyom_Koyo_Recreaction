2e4869777c703f04e336b6399d6ce23a9abb3209
aRonnieAlsop <alsop.arron@gmail.com>
2024-08-16 00:22:13 -0700

    'landing page image flip'

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
 import './../Home/Home.css';
 
+const images = [
+    require('./../../assets/HomeImages/image2.jpg'),
+    require('./../../assets/HomeImages/image3.jpg'),
+    require('./../../assets/HomeImages/image4.jpg'),
+    require('./../../assets/HomeImages/image5.jpg'),
+    require('./../../assets/HomeImages/image6.jpg'),
+    require('./../../assets/HomeImages/image7.jpg'),
+];
+
 const Home = () => {
+    const [currentImage, setCurrentImage] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentImage((prevIndex) => (prevIndex + 1) % images.length);
+        }, 4000);
+
+        return () => clearInterval(interval);
+    }, []);
+
     return (
         <div className="home-container">
-            <img src={require('./../../assets/HomeImages/th.jpg')} alt="Park" className="home-image" />
+            <img src={images[currentImage]} alt="Park" className="home-image" />
         </div>
     );
 };
