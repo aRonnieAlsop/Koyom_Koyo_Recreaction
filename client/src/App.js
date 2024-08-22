19a0d8db904dae83b2b1c24700e499e973d94065
aRonnieAlsop <alsop.arron@gmail.com>
2024-08-22 11:01:55 -0700

    'Header only on Landing Page now'

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,38 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, withRouter } from 'react-router-dom';
 import CreateUser from './components/CreateUser';
 import Users from './components/Users';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home.jsx';
+import NavBar from './components/NavBar/NavBar';
 
-function App() {
+function App({ location }) {
     return (
-      <Router>
-      <div>
-          <Header />
-          <Switch>
-              <Route path="/" exact component={Home} />
-              <Route path="/create-user" component={CreateUser} />
-              <Route path="/users" component={Users} />
-          </Switch>
-      </div>
-  </Router>
+        <div>
+            {/* NavBar is always rendered */}
+            <NavBar />
+            {/* Only render the Header on the Home page */}
+            {location.pathname === '/' && <Header />}
+            <div>
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/create-user" component={CreateUser} />
+                    <Route path="/users" component={Users} />
+                </Switch>
+            </div>
+        </div>
     );
 }
 
-export default App;
+const AppWithRouter = withRouter(App);
+
+export default function AppWrapper() {
+    return (
+        <Router>
+            <AppWithRouter />
+        </Router>
+    );
+}
+
+
 
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
-import NavBar from './../NavBar/NavBar';
 import './Header.css'; 
 
 const Header = () => {
     return (
         <header>
-            <NavBar />
-            <div class="header-container">
-             <h1>KOYOM KOYO</h1>
-            <h2>Parks & Recreation</h2>   
+            <div className="header-container">
+                <h1>KOYOM KOYO</h1>
+                <h2>Parks & Recreation</h2>   
             </div>
-            
         </header>
     );
 };
