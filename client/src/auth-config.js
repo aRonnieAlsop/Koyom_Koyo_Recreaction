3651070574b707a9369cda390193151826f239f6
aRonnieAlsop <alsop.arron@gmail.com>
2024-09-11 21:00:18 -0700

    'Auth0 wrap'

---
+++ client/src/auth-config.js
@@ -0,0 +1,4 @@
+export const authConfig = {
+    domain: 'dev-yvye3foctyqkrxw1.us.auth0.com',
+    clientId: 'hSjUP4H6SYtMPAfn2mO2JvudUGTVG80u'
+};
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css';
 import App from './App';
+import { authConfig } from './auth-config';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Auth0Provider
+      domain={authConfig.domain}
+      clientId={authConfig.clientId}
+      redirectUri={window.location.origin}
+      useRereshTokens={true}
+      cacheLocation="localstorage"
+      >
     <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
