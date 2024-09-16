c78dee0d45eb23a844fd14e33ac6d775ee2cb606
aRonnieAlsop <alsop.arron@gmail.com>
2024-09-15 22:14:37 -0700

    'images for Programs'

--- client/src/components/Programs/Programs.jsx
+++ client/src/components/Programs/Programs.jsx
@@ -1,5 +1,17 @@
-import React from 'react';    
+import React, { useState } from 'react';    
+import './Programs.css';
 
+const programs = [
+    { id: 1, title: 'Swim Lessons', image: './../../assets/ProgramImages/pool_stock_image.jpg', description: 'Details about Swim Lessons' },
+    { id: 2, title: 'Soccer Camp', image: './../../assets/ProgramImages/soccer_stock_image.jpg', description: 'Details about Soccer Camp' },
+    { id: 3, title: 'Tennis Lessons', image: './../../assets/ProgramImages/Tennis_Court_Stock.avif', description: 'Details about Swim Lessons' },
+    { id: 4, title: 'Lego Fair', image: './../../assets/ProgramImages/Lego_Fair_Stock.jpg', description: 'Details about Soccer Camp' },
+    { id: 5, title: 'Pickleball Tournament', image: './../../assets/ProgramImages/Pickleball_Stock_img.png', description: 'Details about Pickleball Tournament' },
+    { id: 6, title: 'Art in the Park', image: './../../assets/ProgramImages/Art_In_The_Park_Stock_Img.jpg', description: 'Details about Art in the Park' },
+    { id: 7, title: 'Men\'s Softball', image: './../../assets/ProgramImages/Mens_Softball_Stock_Img.jpg', description: 'Details about Men\'s Softball' },
+    { id: 8, title: 'Women\'s Softball', image: './../../assets/ProgramImages/Womens_Softball_stock_img.jpg', description: 'Details about Women\'s Softball' },
+    { id: 9, title: 'Robotics Workshop', image: './../../assets/ProgramImages/Robots_Stock_Img_jpg', description: 'Details about Robotics Workshop' },
+];
 
 const Programs = () => {
 
