6625932c7e4ba4a7253a10a52170d702ac9cf880
aRonnieAlsop <alsop.arron@gmail.com>
2024-09-19 13:12:08 -0700

    'init Program model'

---
+++ server/models/Program.jsx
@@ -0,0 +1,24 @@
+import { Model, DataTypes } from 'sequelize';
+const sequelize = require('.../database');
+
+class Program extends Model {}
+
+Program.init({
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
+}, {
+    sequelize,
+    modelName: 'Program',
+});
+
+module.exports = Program;
